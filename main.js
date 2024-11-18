35d7f695d51b478794322f647613d54a1ab7e854
Isha <isha@gmail.com>
2024-11-19 01:02:03 +0530

    imported the getWeather function from the weather file and console its results

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+import { getWeather } from "./weather";
+console.log(getWeather);
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions.timeZone).then(
+  (res) => {
+    if (res.data) {
+      console.log(res.data);
+    } else if (res.error) {
+      console.error("Error getting weather:", res.error);
+    }
+  }
+);
